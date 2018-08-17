96a98f3756abb62eccafacdf356881d2300edb5f
brchen777 <eric790620@gmail.com>
2018-08-17 18:57:15 +0800

    Update total time and add delay

--- cluster.js
+++ cluster.js
@@ -3,41 +3,44 @@
 
     const cluster = require('cluster');
 
-    const arrayCnt = 1000000;
+    const arrayCnt = 10000000;
     const workerCnt = require('os').cpus().length;
     const range = (arrayCnt / workerCnt);
     const initialTime = new Date();
     let finishCnt = 0;
-    let totalTime = 0;
     let allShaResult = [];
 
-    for (let i = 0; i < workerCnt; i++) {
-        cluster.setupMaster({
-            cwd: './res',
-            exec: './c_worker.js'
-        });
-        const worker = cluster.fork({ id: i });
+    const mainFun = () => {
+        for (let i = 0; i < workerCnt; i++) {
+            cluster.setupMaster({
+                cwd: './res',
+                exec: './c_worker.js'
+            });
+            const worker = cluster.fork({ id: i });
 
-        worker.on('message', (msg) => {
-            // get sha256 result from each worker
-            const { shaResult } = msg;
-            allShaResult.push(shaResult);
+            worker.on('message', (msg) => {
+                // get sha256 result from each worker
+                const { shaResult } = msg;
+                allShaResult.push(shaResult);
 
-            const now = new Date();
-            let runTime = now - initialTime;
-            console.log(`[worker ${worker.id} finish]: ${runTime} ms`);
+                console.log(`[worker ${worker.id} finish]`);
 
-            finishCnt++;
-            totalTime += runTime;
-            // merge all sha256 result
-            if (finishCnt === workerCnt) {
-                allShaResult = [].concat(...allShaResult);
-                console.log(`All workers are finish. Total time: ${totalTime} ms`);
-                process.exit(0);
-            }
-        });
-        worker.send({ range });
-    }
+                finishCnt++;
+                // merge all sha256 result
+                if (finishCnt === workerCnt) {
+                    allShaResult = [].concat(...allShaResult);
+
+                    const now = new Date();
+                    console.log(`All workers are finish. Total time: ${now - initialTime} ms`);
+
+                    process.exit(0);
+                }
+            });
+            worker.send({ range });
+        }
+    };
+
+    setTimeout(mainFun, 3000);
 })();
 
 
--- worker_threads.js
+++ worker_threads.js
@@ -3,37 +3,41 @@
 
     const { Worker } = require('worker_threads');
 
-    const arrayCnt = 1000000;
+    const arrayCnt = 10000000;
     const workerCnt = require('os').cpus().length;
     const range = (arrayCnt / workerCnt);
     const initialTime = new Date();
     let finishCnt = 0;
-    let totalTime = 0;
     let allShaResult = [];
 
-    for (let i = 0; i < workerCnt; i++) {
-        const worker = new Worker('./res/wt_worker.js', {
-            workerData: { id: i }
-        });
+    const mainFun = () => {
+        for (let i = 0; i < workerCnt; i++) {
+            const worker = new Worker('./res/wt_worker.js', {
+                workerData: { id: i }
+            });
+        
+            worker.on('message', (msg) => {
+                // get sha256 result from each worker
+                const { shaResult } = msg;
+                allShaResult.push(shaResult);
     
-        worker.on('message', (msg) => {
-            // get sha256 result from each worker
-            const { shaResult } = msg;
-            allShaResult.push(shaResult);
+                console.log(`[worker ${worker.threadId} finish]`);
+    
+                finishCnt++;
+                totalTime += runTime;
+                // merge all sha256 result
+                if (finishCnt === workerCnt) {
+                    allShaResult = [].concat(...allShaResult);
+    
+                    const now = new Date();
+                    console.log(`All workers are finish. Total time: ${now - initialTime} ms`);
+    
+                    process.exit(0);
+                }
+            });
+            worker.postMessage({ range });
+        }
+    };
 
-            const now = new Date();
-            let runTime = now - initialTime;
-            console.log(`[worker ${worker.threadId} finish]: ${runTime} ms`);
-
-            finishCnt++;
-            totalTime += runTime;
-            // merge all sha256 result
-            if (finishCnt === workerCnt) {
-                allShaResult = [].concat(...allShaResult);
-                console.log(`All workers are finish. Total time: ${totalTime} ms`);
-                process.exit(0);
-            }
-        });
-        worker.postMessage({ range });
-    }
+    setTimeout(mainFun, 3000);
 })();
